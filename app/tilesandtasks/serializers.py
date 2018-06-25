38543d06710ee7800572b936b068f46a32fcb845
Matthew Bradley <mjpbradley@gmail.com>
2018-06-25 15:43:10 +0200

    FE updates

--- app/tilesandtasks/serializers.py
+++ app/tilesandtasks/serializers.py
@@ -3,15 +3,24 @@ from .models import Tiles, Tasks
 
 
 class TasksSerializer(serializers.ModelSerializer):
+    tile = serializers.SerializerMethodField()
+
     class Meta:
         model = Tasks
         fields = "__all__"
+
+    def get_tile(self, obj):
+        out = {
+            'date': obj.tile.date,
+            'id': obj.tile.id,
+            'status_live': obj.tile.status_live
+        }
+        return out
 
 
 class TilesSerializer(serializers.ModelSerializer):
     tasks = TasksSerializer(many=True, read_only=True)
     status_live = serializers.SerializerMethodField()
-
 
     class Meta:
         model = Tiles
